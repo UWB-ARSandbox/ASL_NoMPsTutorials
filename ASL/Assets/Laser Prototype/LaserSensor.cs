e96361b17b3ad52a5a63425b3184206320094338
Joshua Sterner <josh@joshuasterner.com>
2021-04-07 19:55:37 -0700

    Add initial laser prototype.
    This commit includes a laser source and beam splitter prefab, a laser
    sensor script, and an initial laser lighting script which adds point
    lights to the laser beam for dynamic glow.

---
+++ ASL/Assets/Laser Prototype/LaserSensor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LaserSensor : MonoBehaviour
+{
+ 
+    public UnityEvent OnBeamIn;
+    public UnityEvent OnBeamOut;
+    private bool laserDetectedLastFrame;
+    private bool laserDetected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void LateUpdate()
+    {
+        if (laserDetected && !laserDetectedLastFrame)
+        {
+            OnBeamIn.Invoke();
+        }
+        if (!laserDetected && laserDetectedLastFrame)
+        {
+            OnBeamOut.Invoke();
+        }
+        laserDetectedLastFrame = laserDetected;
+        laserDetected = false;
+    }
+
+    public void Trigger()
+    {
+        laserDetected = true;
+    }
+    public void TestIn() {
+        GetComponent<MeshRenderer>().material.color = new Color(0.0f, 1.0f, 0.0f);
+    }
+    public void TestOut()
+    {
+        GetComponent<MeshRenderer>().material.color = new Color(1.0f, 0.0f, 0.0f);
+    }
+
+}
