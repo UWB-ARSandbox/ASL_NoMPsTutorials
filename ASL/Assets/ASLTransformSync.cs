876745dded1a2e7877eae8197d3430edc01e979f
Joshua Sterner <josh@joshuasterner.com>
2021-04-15 14:25:09 -0700

    Update ASLTransformSync.

--- ASL/Assets/ASLTransformSync.cs
+++ ASL/Assets/ASLTransformSync.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 public class ASLTransformSync : MonoBehaviour
 {
     private Vector3 remotePosition;
+    private Quaternion remoteRotation;
+    private Vector3 remoteScale;
     private Vector3 previousPosition;
+    private Quaternion previousRotation;
+    private Vector3 previousScale;
     private bool transformChangedRemotely;
-    private int frame;
-    private float remoteFrame;
     private int id;
     // Start is called before the first frame update
     void Start()
@@ -20,19 +22,16 @@ public class ASLTransformSync : MonoBehaviour
             if (id != f[0])
             {
                 remotePosition = new Vector3(f[1], f[2], f[3]);
-                remoteFrame = f[4];
-                Debug.Log("Recieved remote position update. Remote Frame: " + remoteFrame + " remotePosition: " + remotePosition);
+                remoteRotation = new Quaternion(f[4], f[5], f[6], f[7]);
+                remoteScale = new Vector3(f[8], f[9], f[10]);
                 transformChangedRemotely = true;
             }
-            Debug.Log("Remote ID: " + f[0]);
         });
     }
 
     // Update is called once per frame
     void Update()
     {
-        frame++;
-
         GameLiftManager glm = GameLiftManager.GetInstance();
         List<string> usernames = new List<string>();
         foreach (string username in glm.m_Players.Values)
@@ -46,30 +45,39 @@ public class ASLTransformSync : MonoBehaviour
         //     send transform data in float array to be stored in remoteTransform
         // load remoteTransform into local transform if remoteTransform recieved
         // store transform into previousTransform
-        if (transform.localPosition.x != previousPosition.x)
+
+        if (transform.localPosition != previousPosition || transform.localRotation != previousRotation || transform.localScale != previousScale)
         {
-            //Debug.Log("Frame: " + frame + " transformChangedRemotely: " + transformChangedRemotely + "localPosition: " + transform.localPosition
-            //   + " remotePosition: " + remotePosition + " previousPosition: " + previousPosition);
             // position changed locally
             GetComponent<ASLObject>().SendAndSetClaim(() =>
             {
-                float[] f = new float[5];
+                float[] f = new float[11];
                 f[0] = id;
                 f[1] = transform.localPosition.x;
                 f[2] = transform.localPosition.y;
                 f[3] = transform.localPosition.z;
-                f[4] = frame;
+
+                f[4] = transform.localRotation.x;
+                f[5] = transform.localRotation.y;
+                f[6] = transform.localRotation.z;
+                f[7] = transform.localRotation.w;
+
+                f[8] = transform.localScale.x;
+                f[9] = transform.localScale.y;
+                f[10] = transform.localScale.z;
+
                 GetComponent<ASLObject>().SendFloatArray(f);
             });
         }
         if (transformChangedRemotely)
         {
-            //Debug.Log("transformChangedRemotely. Remote Frame: " + remoteFrame);
-            //Debug.Log("Frame: " + frame + " transformChangedRemotely: " + transformChangedRemotely + "localPosition: " + transform.localPosition
-            //   + " remotePosition: " + remotePosition + " previousPosition: " + previousPosition);
             transform.localPosition = remotePosition;
+            transform.localRotation = remoteRotation;
+            transform.localScale = remoteScale;
             transformChangedRemotely = false;
         }
         previousPosition = transform.localPosition;
+        previousRotation = transform.localRotation;
+        previousScale = transform.localScale;
     }
 }
