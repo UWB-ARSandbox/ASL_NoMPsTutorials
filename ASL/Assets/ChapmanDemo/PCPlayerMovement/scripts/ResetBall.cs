a623e872089b6277dcaff2e83c93f2bd122e1614
Kenneth Ven <venk@uw.edu>
2021-04-19 22:38:32 -0700

    Add puzzle mech scene with button, pressure plate

---
+++ ASL/Assets/ChapmanDemo/PCPlayerMovement/scripts/ResetBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBall : MonoBehaviour
+{
+    public Vector3 initPos;
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int layerMask = 1 << 10;
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100, layerMask))
+            {
+                if (hit.transform.name == "ButtonBallReset")
+                {
+                    ball.transform.position = initPos;            
+                }
+            
+            }
+
+        }
+    }
+}
